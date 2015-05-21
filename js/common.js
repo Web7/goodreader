35eb1848731a32d94e76e9b78390ee5014eb653d
Aleksandr Nikitin <a.nikitin@i.ua>
2015-05-21 16:35:04 +0300

    Minor fix

--- js/common.js
+++ js/common.js
@@ -12,6 +12,8 @@
 	};
 
 	var classNames = {
+		textRight: 'text-right',
+
 		grUserManualMenu: 'gr-user-manual-menu',
 		grManualMenu: 'gr-manual-menu',
 
@@ -22,7 +24,12 @@
 		grDefault: 'gr-default',
 		grFixed: 'gr-fixed',
 		grPositionFooter: 'gr-position-footer',
-		grStopFind: 'gr-stop-find'
+		grStopFind: 'gr-stop-find',
+
+		grShowShortText: 'gr-show-short-text',
+		grShowFullText: 'gr-show-full-text',
+		grTextHeight: 'gr-text-height',
+		grShowFullTextButton: 'gr-show-full-text-button'
 	};
 	var ids = {};
 
@@ -48,12 +55,16 @@
 	var $grUserManualMenu,
 		$grFooter,
 		$btnScrollTo,
+		$grShowShortText,
+		$grTextHeight,
 		posGrUserManualMenu;
 
 	$(function(){
 		$grUserManualMenu = $(selectors.grUserManualMenu);
 		$btnScrollTo = $(selectors.btnScrollTo);
 		$grFooter = $(selectors.grFooter);
+		$grShowShortText = $(selectors.grShowShortText);
+		$grTextHeight = $(selectors.grTextHeight);
 
 		if ($grUserManualMenu.exists()) {
 			posGrUserManualMenu = $grUserManualMenu.offset();
@@ -67,6 +78,8 @@
 			});
 			$btnScrollTo.scrollToTop();
 		}
+
+		gr.renderShowShortText();
 	});
 
 	$(document).on('scroll', function(){
@@ -89,6 +102,42 @@
 			}
 
 		}
-
 	});
+
+	$(window).on('resize', function(){
+		gr.renderShowShortText();
+	});
+
+	gr.renderShowShortText = function() {
+		if ($grShowShortText.exists()) {
+			$(selectors.grShowFullTextButton).remove();
+			$grShowShortText.each(function(){
+				var $this = $(this);
+				var $grShowFullTextButton;
+				var fullHeight = $this.find(selectors.grTextHeight).height();
+				$this.removeClass(classNames.grShowFullText).removeAttr('style');
+				if (fullHeight > 125) {
+					$grShowFullTextButton = $('<p/>', {class: classNames.textRight + ' ' + classNames.grShowFullTextButton}).html($('<a/>', {
+						href: '#',
+						text: '...More'
+					}));
+					$grShowFullTextButton.insertAfter($this);
+					$grShowFullTextButton.on('click', function (e) {
+						var _$this = $(this);
+						e.preventDefault();
+						if (!$this.hasClass(classNames.grShowFullText)) {
+							$this.animate({height: fullHeight}, 1000).addClass(classNames.grShowFullText);
+							_$this.find('a').text('Hide');
+						} else {
+							$this.animate({height: 125}, 1000).removeClass(classNames.grShowFullText);
+							_$this.find('a').text('...More');
+						}
+						return false;
+					});
+				} else {
+					$this.addClass(classNames.grShowFullText);
+				}
+			});
+		}
+	};
 })();
